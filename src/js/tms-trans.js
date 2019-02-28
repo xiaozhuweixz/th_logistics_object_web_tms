887942e45a05c869a494e098706ac5943518b47c
Kyo <952393476@qq.com>
2019-02-28 15:23:31 +0800

    view update

---
+++ src/js/tms-trans.js
@@ -0,0 +1,41 @@
+$('#TRANS_ARRTIME_EXP').datetimepicker({
+    language: 'zh',
+    format: 'yyyy-mm-dd',
+    todayBtn: true,
+    autoclose: true
+});
+// 时间选择
+$('#TRANS_DEPART_EXP').datetimepicker({
+    language: 'zh',
+    format: 'yyyy-mm-dd',
+    todayBtn: true,
+    autoclose: true
+});
+// 时间选择
+$('#TRANS_REACH_EXP').datetimepicker({
+    language: 'zh',
+    format: 'yyyy-mm-dd',
+    todayBtn: true,
+    autoclose: true
+});
+// 时间选择
+$('#TRANS_ARRTIME').datetimepicker({
+    language: 'zh',
+    format: 'yyyy-mm-dd',
+    todayBtn: true,
+    autoclose: true
+});
+// 时间选择
+$('#TRANS_DEPART').datetimepicker({
+    language: 'zh',
+    format: 'yyyy-mm-dd',
+    todayBtn: true,
+    autoclose: true
+});
+// 时间选择
+$('#TRANS_REACH').datetimepicker({
+    language: 'zh',
+    format: 'yyyy-mm-dd',
+    todayBtn: true,
+    autoclose: true
+});
