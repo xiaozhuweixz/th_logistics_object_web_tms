17889523156d2c11ab37bb39f272814240a3ae0a
Kyo <952393476@qq.com>
2019-03-07 10:43:59 +0800

    update view

--- src/js/tms-trans-add-table.js
+++ src/js/tms-trans-add-table.js
@@ -39,7 +39,7 @@ var TableInit = function () {
             sortable: false,                     //是否启用排序
             sortOrder: "asc",                   //排序方式
             queryParams: oTableInit.queryParams,//传递参数（*）
-            sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+            sidePagination: "client",           //分页方式：client客户端分页，server服务端分页（*）
             pageNumber: 1,                       //初始化加载第一页，默认第一页
             pageSize: 10,                       //每页的记录行数（*）
             pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
